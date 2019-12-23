279e7fb117f3a9717d84798d824dab01780fc325
romqrq <rjaalbuquerque@gmail.com>
2019-12-23 17:25:17 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+# Importing libraries
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+# Creating instance of Flask
+app = Flask(__name__)
+# Adding Mongo database name and URI linking to that database.
+# URI variable saved as environment variable on GitPod
+app.config["MONGO_DBNAME"] = 'Pawer-db'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+# Creating an instance of PyMongo
+mongo = PyMongo(app)
+
+@app.route('/')
+
+@app.route('/get_user')
+def get_user():
+    return render_template('user_home.html', dogs = mongo.db.Dogs.find())
+    # return 'Hello'
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
