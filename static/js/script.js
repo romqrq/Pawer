1e9cdeec0134d6541ab45cd9b21a2968e20aae4c
romqrq <rjaalbuquerque@gmail.com>
2019-12-30 18:05:01 +0000

    Created JS file

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,23 @@
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
 
-  // Or with jQuery
+$(document).ready(function() {
+        $('.collapsible').collapsible();
+        $('select').material_select();
+        $(".button-collapse").sideNav();
+        $('.carousel').carousel();
+    });
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
 
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
+//  document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, options);
+//   });
+
+ 
+
