305d1d7bb2ab192be6d797f5bd02f4c9d75de26e
romqrq <rjaalbuquerque@gmail.com>
2020-01-07 17:35:12 +0000

    Started adding session cookies structure

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # Importing libraries
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 # Creating instance of Flask
 app = Flask(__name__)
+<=========================================
+app.secret_key = SESSION_KEY
 # Adding Mongo database name and URI linking to that database.
 # URI variable saved as environment variable on GitPod
 app.config["MONGO_DBNAME"] = 'pawer'
@@ -18,17 +20,20 @@ mongo = PyMongo(app)
 
 @app.route('/user_home')
 def user_home():
+    """ Main page where users can choose where to go"""
     return render_template('user_home.html')
 
 #Choosing type of account and adding a new user to database
 @app.route('/register')
 def register():
+    """ Page where users can choose a type of account and register through the specific form"""
     return render_template('register.html')
     
 #CREATE
 #Add dog
 @app.route('/new_dog', methods=['POST'])
 def insert_dog():
+    """ Function to add dogs to the database """
     dogs = mongo.db.dogs
     #Converting form to dictionary for Mongo
     dogs.insert_one(request.form.to_dict())
@@ -38,6 +43,7 @@ def insert_dog():
 #Add user
 @app.route('/new_user', methods=['POST'])
 def insert_user():
+    """ Function to add users to the database """
     users=mongo.db.users
     #Converting form to dictionary for Mongo
     users.insert_one(request.form.to_dict())
@@ -47,6 +53,7 @@ def insert_user():
 #Add service
 @app.route('/new_service', methods=['POST'])
 def insert_service():
+    """ Function to add services to the database """
     services=mongo.db.services
     #Converting form to dictionary for Mongo
     services.insert_one(request.form.to_dict())
@@ -56,6 +63,7 @@ def insert_service():
 #Add store
 @app.route('/new_store', methods=['POST'])
 def insert_store():
+    """ Function to add stores to the database """
     stores=mongo.db.stores
     #Converting form to dictionary for Mongo
     stores.insert_one(request.form.to_dict())
@@ -67,21 +75,25 @@ def insert_store():
 #Find dog
 @app.route('/dogs')
 def get_dogs():
+    """ Function to list dogs contained in the database """
     return render_template('dogs.html', dogs=mongo.db.dogs.find())
 
 #Find user
 @app.route('/users')
 def get_users():
+    """ Function to list users contained in the database """
     return render_template('users.html', users=mongo.db.users.find())
 
 #Find service
 @app.route('/services')
 def get_services():
+    """ Function to list services contained in the database """
     return render_template('services.html', services=mongo.db.services.find())
 
 #Find stores
 @app.route('/stores')
 def get_stores():
+    """ Function to list stores contained in the database """
     return render_template('stores.html', stores=mongo.db.stores.find())
 
 if __name__ == '__main__':
