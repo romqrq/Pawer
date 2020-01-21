95e58667739b30f67c2c6d9c09e728ab304cc739
romqrq <rjaalbuquerque@gmail.com>
2020-01-21 14:53:22 +0000

    Cleanup and tidying up on app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ if path.exists('env.py'):
 # Creating instance of Flask
 app = Flask(__name__)
 # app.secret_key = 'SESSION_KEY'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+app.secret_key = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # Adding Mongo database name and URI linking to that database.
 # URI variable saved as environment variable on GitPod
 app.config["MONGO_DBNAME"] = 'pawer'
@@ -21,19 +22,18 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost')
 # Creating an instance of PyMongo
 mongo = PyMongo(app)
 @app.route('/')
-
-@app.route('/user_home')
+# @app.route('/user_home')
 def user_home():
     """ Main page where users can choose where to go"""
     return render_template('user_home.html')
 
-#Choosing type of account and adding a new user to database
+# Choosing type of account and adding a new user to database
 @app.route('/register')
 def register():
     """ Page where users can choose a type of account and register through the specific form"""
     return render_template('register.html')
 
-#User Login
+# User Login
 @app.route('/login', methods = ['GET', 'POST'])
 def user_login():
     """ Loads page where users can login """
@@ -45,9 +45,9 @@ def user_login():
         form_pwd = request.form['password']
         if user_type == 'user':
             login_user = users.find_one({'_email' : request.form['login_email']})
-            #login not working for non staff members how to return 'is_staff' as 'off' from form?
+            # login not working for non staff members how to return 'is_staff' as 'off' from form?
             
-                # staff_user = str(login_user['is_staff'])
+            # staff_user = str(login_user['is_staff'])
             if login_user:    
                 user_id = str(login_user['_id'])
                 staff_user = str(login_user['is_staff'])    
@@ -58,77 +58,78 @@ def user_login():
                         session['is_staff'] = staff_user
                     return render_template('login.html', user='valid')
         if user_type == 'service':
-            login_user = services.find_one({'_email' : request.form['login_email']})
+            login_user = services.find_one({'_email': request.form['login_email']})
             if login_user:
                 db_pwd = login_user['password']
                 if form_pwd == db_pwd:
                     return render_template('login.html', user='valid')
         if user_type == 'store':
-            login_user = stores.find_one({'_email' : request.form['login_email']})
+            login_user = stores.find_one({'_email': request.form['login_email']})
             if login_user:
                 db_pwd = login_user['password']
                 if form_pwd == db_pwd:
                     return render_template('login.html', user='valid')
 
-        else: return render_template('login.html', user='invalid')
+        else: 
+            return render_template('login.html', user='invalid')
  
     return render_template('login.html')
 
 
-#CREATE
-#Add dog
+# CREATE
+# Add dog
 @app.route('/new_dog', methods=['POST'])
 def insert_dog():
     """ Function to add dogs to the database """
-    #Check if the user already exists in the database
+    # Check if the user already exists in the database
     if request.method == "POST":
         dogs = mongo.db.dogs
-        existing_dog = dogs.find_one({'dog_name' : request.form['dog_name']})
+        existing_dog = dogs.find_one({'dog_name': request.form['dog_name']})
 
         if existing_dog is None:
             dogs.insert_one(request.form.to_dict())
             return redirect(url_for('register'))
         
-        return render_template('register.html', dog = True)
+        return render_template('register.html', dog=True)
 
     return redirect(url_for('user_home'))
 
-#Add user
+# Add user
 @app.route('/new_user', methods=['POST'])
 def insert_user():
     """ Function to add users to the database """
-    users=mongo.db.users
-    #Converting form to dictionary for Mongo
+    users = mongo.db.users
+    # Converting form to dictionary for Mongo
     users.insert_one(request.form.to_dict())
     # if request.form['is_staff']:
     #     users.update({'_id': ObjectId })
-    #Redirecting user to home screen
+    # Redirecting user to home screen
     return redirect(url_for('user_home'))
 
-#Add service
+# Add service
 @app.route('/new_service', methods=['POST'])
 def insert_service():
     """ Function to add services to the database """
-    services=mongo.db.services
-    #Converting form to dictionary for Mongo
+    services = mongo.db.services
+    # Converting form to dictionary for Mongo
     services.insert_one(request.form.to_dict())
-    #Redirecting user to home screen
+    # Redirecting user to home screen
     return redirect(url_for('user_home'))
 
-#Add store
+# Add store
 @app.route('/new_store', methods=['POST'])
 def insert_store():
     """ Function to add stores to the database """
     stores=mongo.db.stores
-    #Converting form to dictionary for Mongo
+    # Converting form to dictionary for Mongo
     stores.insert_one(request.form.to_dict())
-    #Redirecting user to home screen
+    # Redirecting user to home screen
     return redirect(url_for('user_home'))
 
 
-#READ
-#Find dog
-@app.route('/dogs', methods=['GET','POST'])
+# READ
+# Find dog
+@app.route('/dogs', methods=['GET', 'POST'])
 def get_dogs():
     """ Function to list dogs contained in the database """
     # if request.method == 'POST':
@@ -138,80 +139,80 @@ def get_dogs():
 
     return render_template('dogs.html', dogs=mongo.db.dogs.find())
 
-#Find user
+# Find user
 @app.route('/users')
 def get_users():
     """ Function to list users contained in the database """
     return render_template('users.html', users=mongo.db.users.find())
 
-#Find service
+# Find service
 @app.route('/services')
 def get_services():
     """ Function to list services contained in the database """
     return render_template('services.html', services=mongo.db.services.find())
 
-#Find stores
+# Find stores
 @app.route('/stores')
 def get_stores():
     """ Function to list stores contained in the database """
     return render_template('stores.html', stores=mongo.db.stores.find())
 
-#UPDATE
+# UPDATE
 @app.route('/dogs/<usr_id>', methods=['GET', 'POST'])
 def update_dog(usr_id):
-    dogs=mongo.db.dogs
-    dogs.update({'_id' : ObjectId(usr_id)}, 
-    {
-        'dog_name':request.form.get('dog_name'),
-        'dog_breed':request.form.get('dog_breed'),
-        'dog_description':request.form.get('dog_description')
-    })
+    dogs = mongo.db.dogs
+    dogs.update({'_id': ObjectId(usr_id)},
+        {
+            'dog_name': request.form.get('dog_name'),
+            'dog_breed': request.form.get('dog_breed'),
+            'dog_description': request.form.get('dog_description')
+        })
     # return redirect(url_for('get_dogs', edit_dog=mongo.db.dogs.find_one({'_id': usr_id})))
     return redirect(url_for('get_dogs'))
 
 
 @app.route('/user/<usr_id>', methods=['GET', 'POST'])
 def update_user(usr_id):
-    user=mongo.db.users
-    user.update({'_id' : ObjectId(usr_id)},
-    {
-        'first_name':request.form.get('first_name'),
-        'last_name':request.form.get('last_name'),
-        'password':request.form.get('password'),
-        '_email':request.form.get('_email'),
-        'is_staff':request.form.get('is_staff'),
-        'user_description':request.form.get('user_description')
-    })
+    user = mongo.db.users
+    user.update({'_id': ObjectId(usr_id)},
+        {
+            'first_name': request.form.get('first_name'),
+            'last_name': request.form.get('last_name'),
+            'password': request.form.get('password'),
+            '_email': request.form.get('_email'),
+            'is_staff': request.form.get('is_staff'),
+            'user_description': request.form.get('user_description')
+        })
     return redirect(url_for('get_users'))
 
 
 @app.route('/services/<usr_id>', methods=['GET', 'POST'])
-def update_service(user_id):
-    service=mongo.db.services
-    service.update({'_id' : ObjectId(usr_id)}, 
-    {
-        'first_name':request.form.get('first_name'),
-        'last_name':request.form.get('last_name'),
-        'type_of_service':request.form.get('type_of_service'),
-        'service_description':request.form.get('service_description'),
-        '_email':request.form.get('_email'),
-        'password':request.form.get('password')
-    })
+def update_service(usr_id):
+    service = mongo.db.services
+    service.update({'_id': ObjectId(usr_id)}, 
+        {
+            'first_name': request.form.get('first_name'),
+            'last_name': request.form.get('last_name'),
+            'type_of_service': request.form.get('type_of_service'),
+            'service_description': request.form.get('service_description'),
+            '_email': request.form.get('_email'),
+            'password': request.form.get('password')
+        })
     return redirect(url_for('get_service'))
 
 @app.route('/stores/<usr_id>', methods=['GET', 'POST'])
 def update_store(usr_id):
-    store=mongo.db.stores
-    store.update({'_id' : ObjectId(usr_id)},
-    {
-        'store_name':request.form.get('store_name'),
-        'store_address':request.form.get('store_address'),
-        'store_description':request.form.get('store_description'),
-        '_email':request.form.get('_email')
-    })
+    store = mongo.db.stores
+    store.update({'_id': ObjectId(usr_id)},
+        {
+            'store_name': request.form.get('store_name'),
+            'store_address': request.form.get('store_address'),
+            'store_description': request.form.get('store_description'),
+            '_email': request.form.get('_email')
+        })
     return redirect(url_for('get_stores'))
 
-#DELETE
+# DELETE
 @app.route('/dogs/<usr_id>', methods=['GET', 'POST'])
 def delete_dog(usr_id):
     mongo.db.dogs.remove({'_id': ObjectId(usr_id)})
@@ -235,4 +236,4 @@ def delete_store(usr_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=True)           
