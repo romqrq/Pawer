b82280322f57bf4ed0e8b7625b632120246291a8
romqrq <rjaalbuquerque@gmail.com>
2020-01-30 15:16:34 +0000

    Added function to direct user to dashboard and load the information from the active user

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def user_login():
                 db_pwd = login_user['password']
                 if form_pwd == db_pwd:
                     session['user_id'] = user_id
+                    session['user_type'] = 'user'
                     if login_user['is_staff']:
                         session['is_staff'] = login_user['is_staff']
                     else:
@@ -57,6 +58,7 @@ def user_login():
                 db_pwd = login_user['password']
                 if form_pwd == db_pwd:
                     session['user_id'] = str(login_user['_id'])
+                    session['user_type'] = 'service'
                     return render_template('login.html', user='valid')
         if user_type == 'store':
             login_user = stores.find_one({'_email': request.form['login_email']})
@@ -64,6 +66,7 @@ def user_login():
                 db_pwd = login_user['password']
                 if form_pwd == db_pwd:
                     session['user_id'] = str(login_user['_id'])
+                    session['user_type'] = 'store'
                     return render_template('login.html', user='valid')
 
         else: 
@@ -78,10 +81,22 @@ def user_logout():
 
     session.pop('user_id')
     session.pop('is_staff')
+    session.pop['usr_type']
 
     return render_template('index.html')
 
+#DASHBOARD
+@app.route('/dashboard/<usr_id>', methods=['GET', 'POST'])
+def get_dashboard(usr_id):
 
+    if session['user_type'] == 'user':
+        return render_template('dashboard.html', user=mongo.db.users.find_one({'_id': ObjectId(usr_id)}))
+    elif session['user_type'] == 'service':
+        return render_template('dashboard.html', users=mongo.db.services.find())
+    else:
+        return render_template('dashboard.html', users=mongo.db.stores.find_one())
+
+    # return render_template('dashboard.html', user=mongo.db.<usr_type>.find_one({'_id': ObjectId(usr_id)}))
 
 # CREATE
 #Add entry
