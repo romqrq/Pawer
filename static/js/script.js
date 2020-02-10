4105018caec1dbb8b3b5cf2f98795fecdbfd94a9
romqrq <rjaalbuquerque@gmail.com>
2020-02-10 20:30:53 +0000

    Used error lens extension to improve standards on css files

--- static/js/script.js
+++ static/js/script.js
@@ -8,13 +8,10 @@ $(document).ready(function() {
 
 
 $('.form-trigger').click(function(){
-    user_id = $(this).attr('id')
-    form_type = $(this).attr('value')
-    console.log(form_type)
-    console.log($(`#${user_id}.form-box[value=${form_type}]`))
-    $('.form-box').not('.hide').addClass('hide')
-    $(`#${user_id}.form-box[value=${form_type}]`).removeClass('hide').get(0).scrollIntoView(false)
-})
-
- 
-
+    user_id = $(this).attr('id');
+    form_type = $(this).attr('value');
+    // console.log(form_type);
+    // console.log($(`#${user_id}.form-box[value=${form_type}]`));
+    $('.form-box').not('.hide').addClass('hide');
+    $(`#${user_id}.form-box[value=${form_type}]`).removeClass('hide').get(0).scrollIntoView(false);
+});
